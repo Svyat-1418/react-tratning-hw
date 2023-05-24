334e44664356786a43d7fa0c7855189953644a62
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-05-24 23:21:37 +0300

    HW5: add HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import {HashRouter} from "react-router-dom";
 import App from './s1-main/App'
 import reportWebVitals from './reportWebVitals'
 import store from './s2-homeworks/hw10/bll/store'
@@ -10,9 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         {/*для дз 10*/}
+      <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
+      </HashRouter>
     </React.StrictMode>
 )
 
