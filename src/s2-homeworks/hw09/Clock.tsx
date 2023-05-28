b0eeec36fcbfa4f2b47736e0c98be037cf7c2781
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-05-28 14:11:06 +0300

    HW9: fix stringDay, stringMonth change to english representation

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -36,8 +36,8 @@ function Clock() {
     const stringDate = date.toLocaleDateString('ru-RU') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = date.toLocaleDateString('ru-RU', {weekday: 'long'}) || <br/> // пишут студенты
-    const stringMonth = date.toLocaleDateString('ru-RU', {month: 'long'}) || <br/> // пишут студенты
+    const stringDay = date.toLocaleDateString('en-EN', {weekday: 'long'}) || <br/> // пишут студенты
+    const stringMonth = date.toLocaleDateString('en-EN', {month: 'long'}) || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
