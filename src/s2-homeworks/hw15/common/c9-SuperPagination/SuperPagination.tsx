39a384807ca202eddb0e7b8e50025fe458860720
Svyatoslav Nemkovich <svyat5555@gmail.com>
2023-07-09 23:46:36 +0300

    HW15: fix onChangeSelect

--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
 import SuperSelect from '../../../hw07/common/c5-SuperSelect/SuperSelect'
 import {Pagination} from '@mui/material'
 import s from './SuperPagination.module.css'
@@ -23,9 +23,9 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = (
       onChange(page, itemsCountForPage)
     }
 
-    const onChangeSelect = (event: any) => {
+    const onChangeSelect = (event: ChangeEvent<HTMLSelectElement>) => {
         // пишет студент
-      onChange(page, event)
+      onChange(page, +event.currentTarget.value)
     }
 
     return (
